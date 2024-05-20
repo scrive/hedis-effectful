8f21f0f788ac61c3dcf93a7f8ee93e08b5c44a67
Andrzej Rybczak <andrzej@rybczak.net>
2024-05-20 20:25:44 +0200

    Use dynamic dispatch

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hedis-effectful
 
-[![Build Status](https://github.com/scrive/hedis-effectful/workflows/Haskell-CI/badge.svg?branch=master)](https://github.com/scrive/hedis-effectful/actions?query=branch%3Amaster)
+[![Build Status](https://github.com/scrive/hedis-effectful/actions/workflows/haskell-ci.yml/badge.svg?branch=master)](https://github.com/scrive/hedis-effectful/actions?query=branch%3Amaster)
 [![Hackage](https://img.shields.io/hackage/v/hedis-effectful.svg)](https://hackage.haskell.org/package/hedis-effectful)
 [![Dependencies](https://img.shields.io/hackage-deps/v/hedis-effectful.svg)](https://packdeps.haskellers.com/feed?needle=andrzej@rybczak.net)
 [![Stackage LTS](https://www.stackage.org/package/hedis-effectful/badge/lts)](https://www.stackage.org/lts/package/hedis-effectful)
--- src/Effectful/Redis.hs
+++ src/Effectful/Redis.hs
@@ -4,32 +4,30 @@
 -- | Access to a Redis database via 'MonadRedis'.
 module Effectful.Redis
   ( -- * Effect
-    Redis
+    Redis (..)
 
     -- * Handler
   , runRedis
   )
 where
 
-import qualified Database.Redis as R
+import Database.Redis qualified as R
 import Effectful
-import Effectful.Dispatch.Static
+import Effectful.Dispatch.Dynamic
 
 -- | Provide the ability to use the 'R.MonadRedis' instance of 'Eff'.
-data Redis :: Effect
+data Redis :: Effect where
+  LiftRedis :: R.Redis a -> Redis m a
 
-type instance DispatchOf Redis = Static WithSideEffects
-
-newtype instance StaticRep Redis = Redis R.Connection
+type instance DispatchOf Redis = Dynamic
 
 -- | Run the redis effect.
-runRedis :: (IOE :> es) => R.Connection -> Eff (Redis : es) a -> Eff es a
-runRedis = evalStaticRep . Redis
+runRedis :: IOE :> es => R.Connection -> Eff (Redis : es) a -> Eff es a
+runRedis conn = interpret $ \_ -> \case
+  LiftRedis action -> liftIO $ R.runRedis conn action
 
 ----------------------------------------
 -- Orphan instance
 
-instance (IOE :> es, Redis :> es) => R.MonadRedis (Eff es) where
-  liftRedis redis = do
-    Redis connection <- getStaticRep
-    liftIO $ R.runRedis connection redis
+instance Redis :> es => R.MonadRedis (Eff es) where
+  liftRedis = send . LiftRedis
